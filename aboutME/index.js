e3bd1e91d5ecb2cc221e0ea02f6294fdb536a51d
unknown <law9504@gmail.com>
2018-04-08 03:57:29 +0800

    update layout of aboutME

--- aboutME/index.js
+++ aboutME/index.js
@@ -8,6 +8,8 @@ var about_meRouter = require('./routes/about_me');
 app.set('views', path.join(__dirname, 'views'));// 设置存放模板文件的目录
 app.set('view engine', 'ejs');// 设置模板引擎为 ejs
 
+app.use('/public', express.static(__dirname + '/public'));
+
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 app.use('/about_me', about_meRouter);
